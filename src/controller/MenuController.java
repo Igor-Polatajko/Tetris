e5eac26c1dd994f9ddaf8598543159042fb51a32
Igor-Polatajko <igor.bogdanovich39@gmail.com>
2019-02-12 17:30:52 +0200

    Added menu

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MenuController {
+
+    private SceneController sceneController = SceneController.getObj();
+
+    @FXML
+    private Button toGame;
+
+    @FXML
+    private void toGameBtnClick(){
+        sceneController.setSceneState(SceneStates.GAME_FIELD);
+    }
+
+    @FXML
+    private void BestScoresBtnClick(){
+        sceneController.setSceneState(SceneStates.BEST_SCORES);
+    }
+
+    @FXML
+    private void ExitBtnClick(){
+        System.exit(0);
+    }
 }
--- src/controller/SceneController.java
+++ src/controller/SceneController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class SceneController {
@@ -17,6 +21,7 @@ public class SceneController {
 
     public void init(Stage stage) {
         this.stage = stage;
+        stage.setResizable(false);
     }
 
     public void setSceneState(SceneStates sceneState) {
@@ -25,6 +30,34 @@ public class SceneController {
     }
 
     private void handleSceneState() {
+        switch (currentSceneState) {
+            case MENU:
+                Scene menuScene = getMenuScene();
+                stage.setScene(menuScene);
+                stage.show();
+                break;
+            case BEST_SCORES:
 
+                break;
+            case GAME_FIELD:
+
+                break;
+            case GAME_OVER:
+
+                break;
+        }
+    }
+
+
+    private Scene getMenuScene() {
+        Scene menuScene = null;
+        try {
+            VBox root = (VBox) FXMLLoader.load(getClass().getResource("/view/menu.fxml"));
+            menuScene = new Scene(root);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return menuScene != null ? menuScene : new Scene(new Label("Some errors!"), 450, 300);
     }
 }
